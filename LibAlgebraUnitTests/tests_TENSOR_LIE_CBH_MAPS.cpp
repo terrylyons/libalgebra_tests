2843219d7409e2cfaf7dc071d97211aef4bed6e9
Terry Lyons <tlyons@lyonstech.net>
2019-09-17 11:31:47 +0100

    Add TENSOR_LIE_CBH_MAPS added

---
+++ LibAlgebraUnitTests/tests_TENSOR_LIE_CBH_MAPS.cpp
@@ -0,0 +1,276 @@
+/* *************************************************************
+
+Copyright 2010-2019 Terry Lyons, Stephen Buckley, Djalil Chafai,
+Greg Gyurkó and Arend Janssen.
+
+Distributed under the terms of the GNU General Public License,
+Version 3. (See accompanying file License.txt)
+
+************************************************************* */
+// THIS TEST SUITE IS DERIVED FROM LIBALGEBRA-DEMO.cpp
+// BUT EXPLOITS THE MORE UNIFIED SETUP INTERFACE NOW AVAILABLE
+//
+// BREAKING CHANGE in CBH which now consumes vector<const LIE*> not 
+// vector<LIE*> and there is no natural conversion. Change your code.
+
+// libalgebra functionality
+#include "libalgebra/alg_types.h"
+
+// std:: dependencies for current tests
+#include <iostream>
+#include <vector>
+#include <random>
+
+// the unit test framework
+#include <UnitTest++/UnitTest++.h>
+
+// a debugging tool - SHOW(X) outputs variable name X and its content to a stream (e.g. cout) 
+#include "SHOW.h"
+
+
+// to allow redefinitions in other test modules
+namespace {
+	// the tensor shape variables
+	const unsigned DEPTH = 5;
+	const unsigned ALPHABET_SIZE = 2;
+	const unsigned DEPTH2 = 4;
+	const unsigned ALPHABET_SIZE2 = 3;
+	const unsigned STEPS = 30;
+
+	// the number field
+		// IEEE float - SPReal,
+		// IEEE double - DPReal, 
+		// GMP/MPIR rational arithmetic - Rational     
+	const coefficient_t SCALAR_T = Rational;
+
+	/// uni_env - a class defining a single algebraic environment
+	struct uni_env
+	{
+		// types
+		typedef alg_types<DEPTH, ALPHABET_SIZE, SCALAR_T>  ALG;
+		typedef typename ALG::TENSOR TENSOR;
+		typedef typename ALG::LIE LIE;
+		typedef typename ALG::MAPS MAPS;
+		typedef typename ALG::CBH CBH;
+		typedef typename ALG::S S;
+		typedef typename ALG::LET LET;
+
+		// state
+		mutable MAPS maps; // has cached state
+		mutable CBH cbh; // has cached state
+	};
+
+	/// uni_env2 - a class defining a second algebraic environment side by side with the first
+	struct uni_env2 : uni_env
+	{
+		// types
+		typedef alg_types<DEPTH2, ALPHABET_SIZE2, SCALAR_T>  ALG2;
+		typedef typename ALG2::TENSOR TENSOR2;
+		typedef typename ALG2::LIE LIE2;
+		typedef typename ALG2::MAPS MAPS2;
+		typedef typename ALG2::CBH CBH2;
+		typedef typename ALG2::S S2;
+		typedef typename ALG2::LET LET2;
+
+		// state
+		mutable MAPS2 maps2; // has cached state
+		mutable CBH2 cbh2; // has cached state
+	};
+
+
+
+
+	struct categorical_path : uni_env2
+	{
+		// state
+		const size_t steps;
+		const size_t width;
+		std::vector<LIE> increments;
+
+		// accessors
+				// dimensions
+		size_t Steps() const { return steps; }
+		size_t Width() const { return width; }
+
+		// the path data
+		const LIE* begin() const { return &(*increments.begin()); }
+		const LIE* end() const { return begin() + increments.size(); }
+
+		// constructor
+		categorical_path() :
+			steps(STEPS),
+			width(ALPHABET_SIZE),
+			increments((Steps()), LIE())
+		{
+			// set up random number generation
+				// seeded generator	
+			unsigned int seed = (const unsigned int&)0x6d35f0e5b8f6c603;//std::random_device seed; unsigned int seed = seed();
+			std::mt19937 generator;
+			generator.seed(seed);
+
+			// rng
+			std::uniform_int_distribution<LET> distribution(1, ALPHABET_SIZE);
+
+			// create random categorical increments
+			for (int i = 0; i < Steps(); ++i)
+				increments[i] = LIE(distribution(generator), S(1));
+		}
+		// helper functions
+				/// computes a signature from an iterable sequence of lie elements
+		template<class ITERATOR_T>
+		TENSOR signature(ITERATOR_T begin, ITERATOR_T end) const
+		{
+			TENSOR signature(S(1));
+			for (ITERATOR_T i = begin; i != end; i++)
+				signature *= exp(maps.l2t(*i));
+			return signature;
+		}
+
+		/// computes the logsignature from the signature
+		template<class ITERATOR_T>
+		LIE logsignature(ITERATOR_T begin, ITERATOR_T end) const
+		{
+			TENSOR sig = signature(begin, end);
+			return maps.t2l(log(sig));
+		}
+	};
+
+	TEST_FIXTURE(categorical_path, long_multiplication)
+	{
+		auto begin = increments.cbegin();
+		auto end = increments.cend();
+		TENSOR sig = signature(begin, end);
+		for (auto i = begin; i != end; i++) {
+			TENSOR err = sig - signature(begin, i) * signature(i, end);
+			CHECK_EQUAL(TENSOR(), err);
+			size_t l = logsignature(begin, i).size();
+		    size_t r = logsignature(i, end).size();
+			size_t sl = signature(begin, i).size();
+			size_t sr = signature(i, end).size();
+			std::cout << l + r << " " << sl + sr << " ";
+		}
+		std::cout << " : " << LIE::basis.size() << " : " << TENSOR::basis.size() << "\n";
+	}
+
+	TEST_FIXTURE(uni_env, the_basis)
+	{
+		// show the bases:
+		{
+			std::string expected(" 1 2 [1,2] [1,[1,2]] [2,[1,2]] [1,[1,[1,2]]] [2,[1,[1,2]]] [2,[2,[1,2]]] [1,[1,[1,[1,2]]]] [2,[1,[1,[1,2]]]] [2,[2,[1,[1,2]]]] [2,[2,[2,[1,2]]]] [[1,2],[1,[1,2]]] [[1,2],[2,[1,2]]]");
+			std::string ans;
+			for (typename LIE::BASIS::KEY k = LIE::basis.begin(); k != LIE::basis.end();
+				k = LIE::basis.nextkey(k))
+				ans += std::string(" ") + LIE::basis.key2string(k);
+			//CHECK_EQUAL(expected, ans);
+		}
+		{
+			std::string expected(" () (1) (2) (1,1) (1,2) (2,1) (2,2) (1,1,1) (1,1,2) (1,2,1) (1,2,2) (2,1,1) (2,1,2) (2,2,1) (2,2,2) (1,1,1,1) (1,1,1,2) (1,1,2,1) (1,1,2,2) (1,2,1,1) (1,2,1,2) (1,2,2,1) (1,2,2,2) (2,1,1,1) (2,1,1,2) (2,1,2,1) (2,1,2,2) (2,2,1,1) (2,2,1,2) (2,2,2,1) (2,2,2,2) (1,1,1,1,1) (1,1,1,1,2) (1,1,1,2,1) (1,1,1,2,2) (1,1,2,1,1) (1,1,2,1,2) (1,1,2,2,1) (1,1,2,2,2) (1,2,1,1,1) (1,2,1,1,2) (1,2,1,2,1) (1,2,1,2,2) (1,2,2,1,1) (1,2,2,1,2) (1,2,2,2,1) (1,2,2,2,2) (2,1,1,1,1) (2,1,1,1,2) (2,1,1,2,1) (2,1,1,2,2) (2,1,2,1,1) (2,1,2,1,2) (2,1,2,2,1) (2,1,2,2,2) (2,2,1,1,1) (2,2,1,1,2) (2,2,1,2,1) (2,2,1,2,2) (2,2,2,1,1) (2,2,2,1,2) (2,2,2,2,1) (2,2,2,2,2)");
+			std::string ans;
+			for (typename TENSOR::BASIS::KEY k = TENSOR::basis.begin();
+				k != TENSOR::basis.end(); k = TENSOR::basis.nextkey(k))
+				ans += std::string(" (") + TENSOR::basis.key2string(k) +
+				std::string(")");
+			//CHECK_EQUAL(expected, ans);
+
+		}
+	}
+	TEST_FIXTURE(uni_env, constructors)
+	{
+		// construct  
+		   // a scalar
+		S s(3);
+		// letters
+		LET l(1), r(2);
+		// a lie element
+		LIE lie, liel(l,S(1)), liels(l, s);
+		LIE lielr = liels * LIE(r, S(1));
+		// a tensor (constructible on a scalar)
+		TENSOR ten, tens(s), tenls(l, s), tens3 = tenls * TENSOR(r, S(1)) * tenls;
+		std::stringstream ans_stream;
+		ans_stream << l << " " << lie << " " << liel << " " << liels << " " << lielr << " " << ten << " " << tens << " " << tenls << " " << tenls << " " << tens3;
+		std::string expected("1 { } { 1(1) } { 3(1) } { 3([1,2]) } { } { 3() } { 3(1) } { 3(1) } { 9(1,2,1) }");
+		std::string actual = ans_stream.str();
+		CHECK_EQUAL(expected, actual);
+	}
+
+	struct uni_env3 : uni_env2
+	{
+		template <class LIE_T>
+		LIE_T adj(const LIE_T& inner) {
+			return LIE_T(inner);
+		}
+
+		template<class LIE_T, typename ...Ts>
+		LIE_T adj(const LIE_T& outer, Ts...) {
+			return outer * adj(Ts...);
+		}
+	};
+
+	TEST_FIXTURE(uni_env3, LibAlgebraTestLieandCBH)
+	{
+		using std::cout;
+		using std::endl;
+		using std::vector;
+		cout << LIE::basis << endl;
+
+		LIE l1(1, 1), l2(2, 3);
+		cout << "xxx" << (((l2 *= (S)4) *= (l1 * l2 + l1)), l2 = l2 * l2, (l2 += l1)) << endl;
+		cout << maps.l2t(l2) << endl;
+
+		//l2 = LIE(2, 3);
+		//TENSOR t1(maps.l2t(l1)), t2(maps.l2t(l2)), t(t1 + t2 + t1 * t2 - t2 * t1);
+		//cout << "t: " << t << endl;
+		//cout << maps.t2l(t) << " L1 norm is " << (maps.t2l(t)).NormL1() << endl;
+		//cout << maps.t2l(t) << " L1 norm of the second order component is " << (maps.t2l(t)).NormL1(2) << endl;
+
+		//cout << "l2t(t2l) test: " << (t == maps.l2t(maps.t2l((t)))) << endl;
+		//cout << "log(exp) test: " << (t - log(exp(t))) << " " << (t == log(exp(t))) << endl;
+		//cout << "inverse test1: " << (inverse(exp(-t)) - exp(t)) << " " << (inverse(exp(-t)) == exp(t)) << endl;
+		//{
+		//	int f(2);
+		//	cout << "inverse test2: " << inverse(exp(t) * TENSOR(S(f))) * (exp(t) * TENSOR(S(f))) - TENSOR(S(1))
+		//		<< " " << ((inverse(exp(t) * TENSOR(S(2))) * (exp(t) * TENSOR(S(2)))) == TENSOR(S(1)))
+		//		<< endl;
+		//}
+		//cout << "reflect test: " << (inverse(exp(t)) - reflect(exp(t))) << " " << (inverse(exp(t)) == reflect(exp(t))) << endl;
+		//vector<LET> letters;
+		//letters.push_back(1);
+		//letters.push_back(2);
+
+		//cout << "Basic CBH test : " << cbh.basic(letters) << endl;
+		//LIE a(1, 1);
+		//LIE b(2, 1);
+		//vector<const LIE*> v;
+		//v.push_back(&a);
+		//v.push_back(&b);
+		//cout << "Full CBH test : ";
+		//cout << (cbh.full(v) == cbh.basic(letters)) << endl;
+
+		//v.clear();
+		//v.push_back(&l1);
+		//v.push_back(&l2);
+		//cout << "Replace in CBH test: ";
+		//cout << (replace(cbh.basic(letters), letters, v) == cbh.full(v)) << endl;
+
+		//{
+		//	// Compute the coordinate-wise min of two numerical tensors - tricky since the zero values count
+		//	TENSOR tt(t);
+		//	tt += TENSOR(2, 4);
+		//	TENSOR T(t), TT(tt);
+		//	cout << "Compute the coordinate-wise min of two tensors: " << endl;
+		//	cout << t << endl;
+		//	cout << tt << endl;
+		//	cout << (t & tt) << endl;
+		//	cout << (tt & t) << endl;
+		//	cout << (T &= tt) << endl;
+		//	cout << (TT &= t) << endl;
+
+		//	// compute the linear transformation of tensors induced by a map of letters
+		//	auto h = [](LET in)->LET {return in + LET(1); };
+		//	MAPS2::t2t H(h);
+		//	tt *= tt += TENSOR(TENSOR::SCALAR(7));
+		//	cout << tt << "\n" << H(tt) << endl;
+		//}
+	}
+
+}// end of anonymous namespace
