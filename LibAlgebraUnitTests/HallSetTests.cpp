fc153ad7e0516b56424266cc957c03437af91317
Terry Lyons <tlyons@lyonstech.net>
2019-10-08 22:06:52 +0100

    Completlely reworked the Hall set unit tests to a good state

--- LibAlgebraUnitTests/HallSetTests.cpp
+++ LibAlgebraUnitTests/HallSetTests.cpp
@@ -1,173 +1,124 @@
-// FastSigsExp1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// HallSetTests.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 // the libalgebra framework
-#include "libalgebra/alg_types.h"
+#include "alg_framework.h"
 
 // the unit test framework
 #include <UnitTest++/UnitTest++.h>
-#include <iostream>
 
-// simple framework for using lib-algebra
-template <unsigned DEPTH, unsigned ALPHABET_SIZE, enum coefficient_t scalar_t>
-struct alg_framework : alg_types<DEPTH, ALPHABET_SIZE, scalar_t>
-{
-	// static tensor class shape 
-	using alg_types<DEPTH, ALPHABET_SIZE, scalar_t>::DEPTH;
-	using alg_types<DEPTH, ALPHABET_SIZE, scalar_t>::ALPHABET_SIZE;
-	// lib algebra required state
-	mutable typename alg_types<DEPTH, ALPHABET_SIZE, scalar_t>::MAPS maps;
-	mutable typename alg_types<DEPTH, ALPHABET_SIZE, scalar_t>::CBH  cbh;
-	// constructor
-	alg_framework() {}
-};
-
+// validates the hall set and lie multiplication over it
 SUITE(hallset)
 {
-	const unsigned sc1 = 7;
-	typedef alg_framework<sc1, sc1, Rational> SETUP;
-	TEST_FIXTURE(SETUP, hall_set44)
-	{
-		typedef typename std::pair< alg::LET, alg::LET > PARENTS;
-		const std::vector<PARENTS>& hall_set = LIE::basis.hall_set;
-		const auto& degrees = LIE::basis.degrees;
-		const auto& reverse_map = LIE::basis.reverse_map;
+		// DEPTH, ALPHABET SIZE, SCALAR TYPE
+		typedef alg_framework<7, 5, Rational> SETUP75; // takes 143.6  seconds
+		typedef alg_framework<6, 5, Rational> SETUP65; // takes   7.76 seconds
+		typedef alg_framework<5, 7, Rational> SETUP57; // takes  10.89 seconds
 
-		// check it is a hall set https://www.encyclopediaofmath.org/index.php/Hall_set
-	
-		// contains A
-		for (size_t i = 1; i <= ALPHABET_SIZE; ++i)
-			CHECK((std::pair<LET, LET>(0, i) == hall_set[i]));
-
-		// order property
-		for (const auto& k : hall_set)
-			if (k != PARENTS(0, 0))
-				CHECK(k.first < k.second);
-
-		// father in-law property
-		for (const auto& k : hall_set)
-			CHECK(k.first >= hall_set[k.second].first);
-
-		// degrees bounded
-		for (LET i = 1; i < hall_set.size(); ++i)
-			CHECK(degrees[i] < ALPHABET_SIZE + 1);
-
-		// degrees bounded II
-		for (LET i = 1; i < hall_set.size(); ++i)
-			CHECK(degrees[hall_set[i].first] + degrees[hall_set[i].second] == degrees[i] && degrees[i] < DEPTH + 1);
-		for (LET i = 0; i < 1; ++i)
-			CHECK(degrees[i] == 0);
-		for (LET i = 1; i < ALPHABET_SIZE + 1; ++i)
-			CHECK( degrees[i] == 1);
-
-		// If and only if
-		for (LET i = 1; i < hall_set.size(); ++i)
+		TEST_FIXTURE(SETUP65, hall_set_definition)
 		{
-			auto parents = *(hall_set.begin() + i);
-			typename std::map<PARENTS, LET>::const_iterator it;
-			it = reverse_map.find(parents);
-			CHECK(it != reverse_map.end());
-		}
+			// the basis type
+			typedef typename LIE::BASIS BASIS;
+			// the basis instance
+			const BASIS& basis = LIE::basis;
+			// the free lie algebra is a vector space built over a hall basis
+			// the hall basis is built from a collection of binary trees called a hall set
+			// an instance of this basis type is described via an totally ordered sequence of KEYS of type LET starting at 1
+			typedef typename BASIS::KEY KEY;
+			// where each key is connected to exactly two earlier keys, the key's parents
+            typedef typename BASIS::PARENT PARENTS;
+			// each hall basis instance contains one (such tree defining) hall set
+			const std::vector<PARENTS>& hall_set = basis.hall_set;
+			// where the vector is indexed by the keys; the first entry is not a basis element.
+			const auto& reverse_map = LIE::basis.reverse_map;
+			// and we follow Hall in having a degree compatible with the key ordering
+			// basis.degree(k)
 
-		// COMPUTATIONALLY HEAVY
-		for (LET i = 1; i < hall_set.size(); ++i)
-			for (LET j = i+1; j < hall_set.size(); ++j)
-		{
-				if (i < hall_set[j].first) {
-					PARENTS parents = PARENTS(i, j);
-					typename std::map<PARENTS, LET>::const_iterator it;
+			// check the axioms of a hall set https://www.encyclopediaofmath.org/index.php/Hall_set
+
+			// contains the god element
+			CHECK((std::pair<LET, LET>(0, 0))== (hall_set[0]));
+			
+			// contains A
+			for (size_t i = 1; i <= ALPHABET_SIZE; ++i)
+				CHECK((std::pair<LET, LET>(0, i))==(hall_set[i]));
+
+			// order property
+			for (const PARENTS& k : hall_set)
+				if (k != PARENTS(0, 0))
+					CHECK(k.first < k.second);
+
+			// husband younger than father in-law property
+			for (const auto& k : hall_set)
+				CHECK(k.first >= hall_set[k.second].first);
+
+			// sanity tests
+
+			// non-leaf strictly younger property (sanity test - follows from the above)
+			for (LET i = 1 + ALPHABET_SIZE; i < hall_set.size(); ++i)
+				CHECK((hall_set[i].second) < i);
+
+			// test degree additivity and monotonicity
+			// initial values
+			for (LET i = 0; i < 1; ++i)
+				CHECK_EQUAL(0, basis.degree(i));
+			for (LET i = 1; i < ALPHABET_SIZE + 1; ++i)
+				CHECK_EQUAL(1, basis.degree(i));
+			// additivity
+			for (LET i = 1; i < hall_set.size(); ++i)
+				CHECK(basis.degree(hall_set[i].first) + basis.degree(hall_set[i].second) == basis.degree(i));
+			// monotonicity
+			for (LET i = 1; i < hall_set.size(); ++i)
+				CHECK(basis.degree(i - 1) <= basis.degree(i));
+
+			// test grown_up enough
+			CHECK(basis.degree(hall_set.size() - 1) >= DEPTH);
+
+			// Check integrity of lie product and the reverse map on hall elements
+
+			{
+				typename std::vector<PARENTS>::const_iterator begin = hall_set.begin();
+				typename std::map<PARENTS, LET>::const_iterator it;
+
+				// every hall set element is in the reverse map
+				for (LET i = 1; i < hall_set.size(); ++i)
+				{
+					PARENTS parents = *(begin + i);
 					it = reverse_map.find(parents);
-					CHECK(it == reverse_map.end());
+					CHECK(it != reverse_map.end());
+					if (basis.degree(i) <= DEPTH) {
+						CHECK(LIE(i) == LIE(parents.first) * LIE(parents.second));
+					}
+					else {
+						CHECK(LIE() == LIE(parents.first) * LIE(parents.second));
+					}
 				}
+			}
+
+			// COMPUTATIONALLY HEAVY: 
+			// exhaustively confirm recursion for prod always terminates
+
+			if (hall_set.size() * hall_set.size() < (1ULL << 31))
+				for (LET i = 1; i < hall_set.size(); ++i)
+					for (LET j = i + 1; j < hall_set.size(); ++j) {
+						// construct from LET type with constant one 
+						LIE k = LIE(i) * LIE(j);// NON empty homogeneous
+
+						if (basis.degree(i) + basis.degree(j) > DEPTH)
+							CHECK(LIE()== k);
+						else {
+							CHECK(k.size() > 0);
+							for (const auto& p : k)
+								CHECK(basis.degree(p.first) == basis.degree(i) + basis.degree(j));
+						}
+
+						// check that items not in the hall basis are not in reversemap
+						if (i < hall_set[j].first) {
+							PARENTS parents = PARENTS(i, j);
+							typename std::map<PARENTS, LET>::const_iterator it;
+							it = reverse_map.find(parents);
+							CHECK(it == reverse_map.end());
+						}
+					}
 		}
-	}
-
-
-	const int scale = 6;
-	const int depth = 6;
-	const int width = 6;
-	const int STEPS = 6;
-
-	//struct categorical_path : alg_types<depth, width, Rational>
-	//{
-	//	// lib algebra required state
-	//	mutable MAPS maps;
-	//	mutable CBH  cbh;
-	//	// state
-	//	const size_t steps;
-	//	const size_t width;
-	//	const size_t depth;
-	//	std::vector<LIE> increments;
-	//
-	//
-	//	// accessors
-	//			// dimensions
-	//	size_t Steps() const { return steps; }
-	//	size_t Width() const { return width; }
-	//	size_t Depth() const { return depth; }
-	//
-	//	// the path data
-	//	const LIE* begin() const { return &(*increments.begin()); }
-	//	const LIE* end() const { return &(*increments.begin()) + increments.size(); }
-	//
-	//	// constructor
-	//	categorical_path() :
-	//		steps(STEPS),
-	//		width(ALPHABET_SIZE), //constant set in alg_types
-	//		depth(DEPTH), //constant set in alg_types
-	//		increments(Steps(), LIE())
-	//	{
-	//		// set up random number generation
-	//			// seeded generator	
-	//		unsigned int seed = (const unsigned int&)0x6d35f0e5b8f6c603;//std::random_device seed; unsigned int seed = seed();
-	//		std::mt19937 generator;
-	//		generator.seed(seed);
-	//
-	//		// rng
-	//		std::uniform_int_distribution<LET> distribution(1, ALPHABET_SIZE);
-	//
-	//		// create random categorical increments
-	//		for (int i = 0; i < Steps(); ++i)
-	//			increments[i] = LIE(distribution(generator), S(1));
-	//	}
-	//	// helper functions
-	//			/// computes a signature from an iterable sequence of lie elements
-	//	template<class ITERATOR_T>
-	//	TENSOR signature(ITERATOR_T begin, ITERATOR_T end) const
-	//	{
-	//		TENSOR signature(S(1));
-	//		for (ITERATOR_T i = begin; i != end; i++)
-	//			signature *= exp(maps.l2t(*i));
-	//		return signature;
-	//	}
-	//
-	//	/// computes the logsignature from the signature
-	//	template<class ITERATOR_T>
-	//	LIE logsignature(ITERATOR_T begin, ITERATOR_T end) const
-	//	{
-	//		TENSOR sig = signature(begin, end);
-	//		return maps.t2l(log(sig));
-	//	}
-	//};
-	//TEST_FIXTURE(categorical_path,bug)
-	//{
-	//	//4: { 1(4) }
-	//	//446 : { 1([3, [2, [2, [2, 3]]]]) }
-	//
-	//	LIE l1(1), l2(2), l3(3), l4(4);
-	//	LIE k1(l4);
-	//	LIE k2(l3*(l2*(l2*(l2*(l3)))));
-	//	LIE ans = k1 * k2;
-	//}
-	//
-	//TEST_FIXTURE(categorical_path, short_lattice_path_high_dimension)
-	//{
-	//	categorical_path p;
-	//	categorical_path::TENSOR sig = p.signature(p.begin(), p.end());
-	//	auto u = p.maps.t2l(log(sig));
-	//	//categorical_path::LIE logsig = p.maps.t2l(log(sig));
-	//
-	//	//std::cout << "Support for the logsignature:" << logsig.size() << " and for the signature:" << sig.size() << ", the dimension of the ambient tensor algebra is:" << categorical_path::TENSOR::basis.size() << "\n";
-	//}
-	// EXPECTED OUTPUT Double 38 seconds Rational 43 seconds: 1965 19173961
 }
