4d1e0f17a7790cf2689ac15a5d707216b8ba8d02
Sam Morley <41870650+inakleinbottle@users.noreply.github.com>
2021-02-05 10:10:10 +0000

    Added more tests for tensor product

--- LibAlgebraUnitTests/tensor/test_tensor_multiplication.cpp
+++ LibAlgebraUnitTests/tensor/test_tensor_multiplication.cpp
@@ -2,6 +2,8 @@
 // Created by sam on 04/02/2021.
 //
 
+#include <random>
+
 #include <UnitTest++/UnitTest++.h>
 
 #include <libalgebra/libalgebra.h>
@@ -29,6 +31,15 @@ SUITE (tensor_multiplication) {
 
         template <std::size_t N>
         TKEY make_key(std::array<LET, N> arg)
+        {
+            TKEY k;
+            for (std::size_t i=0; i<N; ++i) {
+                k.push_back(arg[i]);
+            }
+            return k;
+        }
+
+        TKEY make_key(const LET* arg, const std::size_t N)
         {
             TKEY k;
             for (std::size_t i=0; i<N; ++i) {
@@ -96,6 +107,77 @@ SUITE (tensor_multiplication) {
         TENSOR expected(make_key(ek));
 
         CHECK_EQUAL(expected, lhs * rhs);
+    }
+
+
+    TEST_FIXTURE(Fixture, test_product_unidim_deg_3_deg_3_overflow_removed) {
+        TEST_DETAILS();
+
+        std::array<LET, 3> k1 {1, 2, 3};
+        std::array<LET, 3> k2 {1, 2, 3};
+        TENSOR lhs(make_key(k1)), rhs(make_key(k2));
+
+        TENSOR expected(tzero);
+
+        CHECK_EQUAL(expected, lhs * rhs);
+    }
+
+    TEST_FIXTURE(Fixture, test_product_multiple_terms) {
+        TEST_DETAILS();
+
+        TENSOR lhs(tunit), rhs(tunit);
+
+        std::array<LET, 5> letters {1, 2, 3, 4, 4};
+        // 1() + 1(1) + 1(1,2) + 1(1,2,3) + 1(1,2,3,4)
+        for (DEG d=1; d<5; ++d) {
+            lhs += TENSOR(make_key(&letters[0], d));
+        }
+
+        // 1() + 1(4) + 1(3,4) + 1(2,3,4) + 1(1,2,3,4)
+        for (DEG d=1; d<5; ++d) {
+            rhs += TENSOR(make_key(&letters[4-d], d));
+        }
+
+        TENSOR expected(tunit);
+
+        // DEG 5
+        expected += TENSOR(make_key(letters));
+        letters = {1,2,3,3,4};
+        expected += TENSOR(make_key(letters));
+        letters = {1,2,2,3,4};
+        expected += TENSOR(make_key(letters));
+        letters = {1,1,2,3,4};
+        expected += TENSOR(make_key(letters));
+
+        letters = {1,2,3,4,0};
+        // (1,2,3,4) = () * (1,2,3,4), (1) * (2,3,4), (1,2) * (3,4), (1,2,3) * (4), (1,2,3,4) * ()
+        expected.add_scal_prod(make_key(&letters[0], 4), S(5));
+
+        // DEG 3
+        letters = {1,2,3,0,0};
+        expected += TENSOR(make_key(&letters[0], 3));
+        letters = {1,2,4,0,0};
+        expected += TENSOR(make_key(&letters[0], 3));
+        letters = {1,3,4,0,0};
+        expected += TENSOR(make_key(&letters[0], 3));
+        letters = {2,3,4,0 ,0};
+        expected += TENSOR(make_key(&letters[0], 3));
+
+        // DEG 2
+        letters = {1,2,0,0,0};
+        expected += TENSOR(make_key(&letters[0], 2));
+        letters = {1,4,0,0,0};
+        expected += TENSOR(make_key(&letters[0], 2));
+        letters = {3,4,0,0,0};
+        expected += TENSOR(make_key(&letters[0], 2));
+
+        // DEG 1
+        expected += TENSOR(TKEY(LET(1)));
+        expected += TENSOR(TKEY(LET(4)));
+
+
+        CHECK_EQUAL(expected, lhs * rhs);
+
     }
 
 }
