82368ca32d2f48eebcc416c162cc6dfbeaca5124
Sam Morley <41870650+inakleinbottle@users.noreply.github.com>
2021-02-09 15:03:39 +0000

    Added dense vector tests, extended simple_basis.h

--- LibAlgebraUnitTests/simple_basis.h
+++ LibAlgebraUnitTests/simple_basis.h
@@ -4,17 +4,28 @@
 
 #ifndef LIBALGEBRAUNITTESTS_SIMPLE_BASIS_H
 #define LIBALGEBRAUNITTESTS_SIMPLE_BASIS_H
+#include <libalgebra/libalgebra.h>
+#include <libalgebra/basis/basis.h>
 
 #include <iostream>
 #include <utility>
+#include <functional>
 
-template <unsigned _D, typename _R>
+using alg::DIMN;
+using alg::DEG;
+
+template <unsigned D, typename R>
 class SimpleIntegerBasis
 {
 public:
-    static const unsigned dimension = _D;
+    static const unsigned dimension = D;
     typedef unsigned KEY;
-    typedef _R RATIONAL;
+    typedef R RATIONAL;
+
+public:
+    // Property tags
+    typedef alg::basis::without_degree degree_tag;
+    typedef alg::basis::ordered<std::less<KEY>> ordering_tag;
 
 
     friend std::ostream& operator<<(std::ostream& os,
@@ -22,6 +33,18 @@ public:
     {
         return (os << arg.second);
     }
+
+    DIMN key_to_index(const KEY k)
+    {
+        return static_cast<DIMN>(k);
+    }
+
+    KEY index_to_key(const DIMN idx)
+    {
+        return static_cast<KEY>(idx);
+    }
+
+
 
 };
 
