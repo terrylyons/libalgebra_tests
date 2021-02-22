88421d154b7a9357cbc2597b923f7cf78f070437
Sam Morley <41870650+inakleinbottle@users.noreply.github.com>
2021-02-22 20:38:34 +0000

    Added new const vector element access tests

--- LibAlgebraUnitTests/vector/vector_element_access_suite.h
+++ LibAlgebraUnitTests/vector/vector_element_access_suite.h
@@ -18,9 +18,28 @@
             REQUIRE CHECK_EQUAL(S(0), neut[i]);
     }
 
+    TEST_FIXTURE(Fixture, test_const_neutral_access_all_zero) {
+        TEST_DETAILS();
+        const VECT neut;
+
+        for (KEY i=0; i<BASIS::dimension; ++i) {
+            REQUIRE CHECK_EQUAL(S(0), neut[i]);
+        }
+
+    }
+
     TEST_FIXTURE(Fixture, test_unidim_set_element) {
         TEST_DETAILS();
         VECT vect(KEY(0));
+
+        CHECK_EQUAL(S(1), vect[KEY(0)]);
+        for (KEY i=1; i<BASIS::dimension; ++i)
+            REQUIRE CHECK_EQUAL(S(0), vect[i]);
+    }
+
+    TEST_FIXTURE(Fixture, test_const_unidim_set_element) {
+        TEST_DETAILS();
+        const VECT vect(KEY(0));
 
         CHECK_EQUAL(S(1), vect[KEY(0)]);
         for (KEY i=1; i<BASIS::dimension; ++i)
