263346dcdec84f68bcaeea0ff8b14a79e255ec51
Sam Morley <41870650+inakleinbottle@users.noreply.github.com>
2021-03-24 11:21:15 +0000

    more tensor tests.

---
+++ LibAlgebraUnitTests/tensor/tensor_exp_suite.h
@@ -0,0 +1,90 @@
+
+    TEST_FIXTURE(fixture, tensor_exp_zero) {
+        TEST_DETAILS();
+        TENSOR arg;
+        TENSOR expected(S(1));
+
+        CHECK_EQUAL(expected, exp(arg));
+    }
+
+    TEST_FIXTURE(fixture, tensor_exp_unit) {
+        TEST_DETAILS();
+
+        TENSOR arg(S(1));
+        TENSOR expected(exp_to_depth(S(1), S(1)));
+
+        CHECK_VEC_CLOSE(expected, exp(arg), expected_error);
+    }
+
+    TEST_FIXTURE(fixture, tensor_exponential_single_letter) {
+        TEST_DETAILS();
+
+        KEY key(1UL);
+        TENSOR arg(key);
+
+        TENSOR expected(exp_to_depth(arg, S(1)));
+
+        TENSOR result(exp(arg));
+
+        CHECK_EQUAL(6, result.size());
+        CHECK_VEC_CLOSE(expected, result, expected_error);
+    }
+
+    TEST_FIXTURE(fixture, tensor_exp_two_letters) {
+        TEST_DETAILS();
+
+        KEY key1(1UL), key2(2UL);
+
+        TENSOR arg;
+        arg.add_scal_prod(key1, S(0.256147));
+        arg.add_scal_prod(key2, S(0.852625));
+        TENSOR result (exp(arg));
+
+        TENSOR expected(exp_to_depth(arg, S(1)));
+
+        // 63 = 1 + 2 + 4 + 8 + 16 + 32
+        CHECK_EQUAL(63, result.size());
+        CHECK_VEC_CLOSE(expected, result, expected_error);
+
+    }
+
+    TEST_FIXTURE(fixture, tensor_exp_degree_2_key) {
+        TEST_DETAILS();
+
+        KEY key(1UL);
+        key.push_back(2UL);
+
+        TENSOR arg;
+        arg.add_scal_prod(key, S(0.534642));
+
+        // exp should contain 1, (1,2), (1,2,1,2)
+        TENSOR result(exp(arg));
+
+        TENSOR expected(exp_to_depth(arg, S(1)));
+
+        CHECK_EQUAL(3, result.size());
+        CHECK_VEC_CLOSE(expected, result, expected_error);
+    }
+
+    TEST_FIXTURE(fixture, tensor_exp_degree_2_2_keys) {
+        TEST_DETAILS();
+
+        KEY key1(1UL), key2(3UL);
+        key1.push_back(2UL);
+        key2.push_back(4UL);
+
+        TENSOR arg;
+        arg.add_scal_prod(key1, S(0.534642));
+        arg.add_scal_prod(key2, S(0.163781));
+
+        // exp should contain 1, (1,2), (3,4), (1,2,1,2), (1,2,3,4), (3,4,1,2), (3,4,3,4)
+        TENSOR result(exp(arg));
+
+        TENSOR expected(exp_to_depth(arg, S(1)));
+
+        CHECK_EQUAL(7, result.size());
+        CHECK_VEC_CLOSE(expected, result, expected_error);
+    }
+
+
+
---
+++ LibAlgebraUnitTests/tensor/test_dense_tensor_functions.cpp
@@ -0,0 +1,124 @@
+//
+// Created by sam on 23/03/2021.
+//
+
+#include <vector>
+
+#include <UnitTest++/UnitTest++.h>
+
+#include <libalgebra/libalgebra.h>
+#include <libalgebra/alg_types.h>
+
+#include "../time_and_details.h"
+#include "../helpers.h"
+
+
+
+
+template <typename Coeff, unsigned Width, unsigned Depth>
+struct fixture
+{
+
+    typedef typename Coeff::S S;
+    typedef typename Coeff::Q Q;
+
+    typedef alg::free_tensor_basis<S, Q, Width, Depth> TBASIS;
+    typedef alg::vectors::dense_vector<TBASIS, Coeff, std::vector<S> > VECT;
+    typedef alg::free_tensor<S, Q, Width, Depth, VECT> TENSOR;
+
+    typedef typename TBASIS::KEY KEY;
+
+    template<class T>
+    T exp_to_depth(T x, S one) {
+        T result (one);
+        T tone(S(1));
+
+        for (size_t i=Depth; i>=1; --i) {
+            result *= (x / Q(i));
+            result += tone;
+        }
+        return result;
+    }
+
+    TENSOR log_to_depth(const TENSOR& x) {
+        TENSOR result;
+
+        KEY kunit;
+        TENSOR tunit(kunit);
+
+        TENSOR xx(x);
+        typename TENSOR::iterator it(xx.find(KEY()));
+        if (it != xx.end()) {
+            xx.erase(it);
+        }
+
+
+        for (unsigned i = Depth; i >= 1; --i) {
+
+            if (i % 2 == 0) {
+                result -= (tunit / Q(i));
+            } else {
+                result += (tunit / Q(i));
+            }
+            result *= xx;
+        }
+        return result;
+    }
+
+};
+
+
+
+SUITE(dense_tensor_functions_float) {
+
+    struct float_field {
+        typedef float S;
+        typedef float Q;
+    };
+
+    static const double expected_error = 3e-8f;
+    typedef fixture<float_field, 5, 5> fixture;
+
+#include "tensor_exp_suite.h"
+#include "tensor_log_suite.h"
+#include "tensor_inverse_suite.h"
+#include "tensor_roundtrip_suite.h"
+
+
+}
+
+
+SUITE(dense_tensor_functions_double) {
+
+    struct double_field {
+        typedef double S;
+        typedef double Q;
+    };
+
+    typedef fixture<double_field, 5, 5> fixture;
+    static const double expected_error = 2e-15;
+
+#include "tensor_exp_suite.h"
+#include "tensor_log_suite.h"
+#include "tensor_inverse_suite.h"
+#include "tensor_roundtrip_suite.h"
+}
+
+
+
+
+SUITE(dense_tensor_functions_rational) {
+
+    struct rational_field {
+        typedef mpq_class S;
+        typedef mpq_class Q;
+    };
+
+    typedef fixture<rational_field, 5, 5> fixture;
+    static const double expected_error = 0.0;
+
+#include "tensor_exp_suite.h"
+#include "tensor_log_suite.h"
+#include "tensor_inverse_suite.h"
+#include "tensor_roundtrip_suite.h"
+}
